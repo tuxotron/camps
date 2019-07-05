846a6412bb1eeebfd41b5dec7d3bb2125075fed6
Tuxotron <tuxotron@cyberhades.com>
2019-07-05 10:54:27 -0400

    add queries

--- camp.go
+++ camp.go
@@ -7,38 +7,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/thedevsaddam/gojsonq"
 )
 
-type Camps struct {
-	Camps []Camp `json:"camps"`
-}
+var campsBytes []byte
 
-// User struct which contains a name
-// a type and a list of social links
-type Camp struct {
-	Name    string `json:"name"`
-	Street1 string `json:"street1"`
-	Street2 string `json:"street2"`
-	City    string `json:"city"`
-	State   string `json:"state"`
-	Zip     string `json:"zip"`
-	Phone   string `json:"phone"`
-	Fax     string `json:"fax"`
-}
+func campsEndPoint(w http.ResponseWriter, r *http.Request) {
 
-var camps Camps
+	jq := gojsonq.New().FromString(string(campsBytes))
 
-func campPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Camp!")
-}
+	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing form: %s", err)
+		return
+	}
 
-func icePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ICE")
+	state := r.Form.Get("state")
+
+	var res interface{}
+	if len(state) > 0 {
+		fmt.Println(state)
+		res = jq.From("camps").Where("state", "=", state).Get()
+	} else {
+		res = jq.From("camps").Get()
+	}
+
+	js, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)
 }
 
 func handleRequests() {
-	http.HandleFunc("/camp", campPage)
-	http.HandleFunc("/ice", icePage)
+	http.HandleFunc("/camps/", campsEndPoint)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -47,22 +50,12 @@ func loadCamps() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened users.json")
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	json.Unmarshal(byteValue, &camps)
+	campsBytes, _ = ioutil.ReadAll(jsonFile)
 }
 
 func main() {
-
 	loadCamps()
-
-	for i := 0; i < len(camps.Camps); i++ {
-		fmt.Println("Camp Name: " + camps.Camps[i].Name)
-	}
-
 	handleRequests()
 }
